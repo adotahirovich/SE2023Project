ef4a92c8f00992b3e0eaa54ae4d53fcbec7a7ea0
adotahirovich <80359702+adotahirovich@users.noreply.github.com>
2023-06-21 00:58:23 +0200

    Update webpack.config.js

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -3,17 +3,22 @@ const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   mode: "development",
-  entry: path.join(__dirname, "./src/index.js"),
+  entry: path.join(dirname, "./src/index.js"),
   output: {
-    path: path.join(__dirname, "/dist"),
+    path: path.join(dirname, "/dist"),
     filename: "main.js",
     publicPath: "/dist/",
   },
   externals: [nodeExternals()],
+  resolve: {
+    fallback: {
+      "path": require.resolve("path-browserify"),
+    },
+  },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /.js$/,
         exclude: /node_modules/,
         use: ["babel-loader"],
       },
